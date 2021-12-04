8d24649e999c4cd98ba7444e6d02d73f49acc38f
Hessel Prins <hessel_prins@hotmail.com>
2021-12-04 15:53:21 +0100

    Day 2 complete - Java

--- Java/src/day2/Puzzle.java
+++ Java/src/day2/Puzzle.java
@@ -14,10 +14,43 @@ public class Puzzle extends day0.Puzzle {
     }
 
     public long part1() {
+        int[] position = new int[2];
 
-        for (String in : input){
-            System.out.println(in);
+        for (String command: input){
+            String[] values = command.split(" ");
+            switch (values[0]) {
+                case "forward":
+                    position[0] += Integer.parseInt(values[1]);
+                    break;
+                case "down":
+                    position[1] += Integer.parseInt(values[1]);
+                    break;
+                case "up":
+                    position[1] -= Integer.parseInt(values[1]);
+                    break;
+            }
         }
-        return 0;
+        return position[0] * position[1];
+    }
+
+    public long part2() {
+        int[] position = new int[3];
+
+        for (String command: input){
+            String[] values = command.split(" ");
+            switch (values[0]) {
+                case "forward":
+                    position[0] += Integer.parseInt(values[1]);
+                    position[1] += position[2] * Integer.parseInt(values[1]);
+                    break;
+                case "down":
+                    position[2] += Integer.parseInt(values[1]);
+                    break;
+                case "up":
+                    position[2] -= Integer.parseInt(values[1]);
+                    break;
+            }
+        }
+        return position[0] * position[1];
     }
 }
